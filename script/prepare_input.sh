0c6ca29a12b55f9eabc135e98778936508275ea5
sparonuzzi <sparonuzzi@mercator-ocean.fr>
2024-08-22 15:44:58 +0200

    created a loop to save lines and errors

--- script/prepare_input.sh
+++ script/prepare_input.sh
@@ -5,19 +5,16 @@ INPUT_DIR=%HPCROOTDIR%/INPUT
 # Create input dir, to be reused from run to run
 mkdir -p ${INPUT_DIR}
 
-if [ ! -s ${INPUT_DIR}/ORCA2_ICE_v4.2.tar  ]; then
-  wget https://zenodo.org/records/3767939/files/ORCA2_ICE_v4.2.tar -P ${INPUT_DIR} 
-fi
-if [ ! -s ${INPUT_DIR}/ORCA2_ICE_v4.2.tar  ]; then
-  tar -xf ${INPUT_DIR}/ORCA2_ICE_v4.2.tar  -C ${INPUT_DIR}/
-  gunzip ${INPUT_DIR}/*.gz
-fi
-
-if [ ! -s ${INPUT_DIR}/INPUTS_PISCES_v4.2.tar ]; then
-  wget https://zenodo.org/records/3767939/files/INPUTS_PISCES_v4.2.tar  -P ${INPUT_DIR}
-fi
-if [ ! -s ${INPUT_DIR}/bathy.orca.nc ]; then
-  tar -xf ${INPUT_DIR}/INPUTS_PISCES_v4.2.tar -C ${INPUT_DIR}/
-  gunzip ${INPUT_DIR}/*.gz
-fi
-rm ${INPUT_DIR}/*.tar
+for FILE_NAME in ORCA2_ICE_v4.2 INPUTS_PISCES_v4.2
+do 
+  if [ ! -f ${INPUT_DIR}/${FILE_NAME}.tar  ]; then
+    wget https://zenodo.org/records/3767939/files/${FILE_NAME}.tar -P ${INPUT_DIR}
+  fi
+  if [ ! -d ${INPUT_DIR}/${FILE_NAME}  ]; then
+    mkdir ${INPUT_DIR}/${FILE_NAME}
+    tar -xf ${INPUT_DIR}/${FILE_NAME}.tar  -C ${INPUT_DIR}/${FILE_NAME}
+    gunzip ${INPUT_DIR}/${FILE_NAME}/*.gz
+    ln -s ${INPUT_DIR}/${FILE_NAME}/*.nc ${INPUT_DIR}
+  fi
+done
+# rm ${INPUT_DIR}/*.tar
